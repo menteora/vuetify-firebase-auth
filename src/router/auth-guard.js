7dcd1701b9f0a115fd9f4399f21a5e24391cbb58
Luca D'Amico <menteora@gmail.com>
2018-01-09 14:42:30 +0100

    removed console.log

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,15 +1,7 @@
 import store from '../store'
 
 export default (to, from, next) => {
-  console.log(store.getters.user)
-  console.log(to.fullPath)
-  if (to.fullPath === '/logout') {
-    console.log('called')
-    store.dispatch('logout')
-  } else {
-    console.log('not called')
-  }
-  console.log(store.getters.user)
+  if (to.fullPath === '/logout') { store.dispatch('logout') }
   if (store.getters.user) {
     next()
   } else {
